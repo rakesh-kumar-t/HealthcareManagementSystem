11d15430b3fe76a544c4f4dc7518182eda7ea921
EPPAKAYALA MOUNIKA <mounika.ep@gmail.com>
2020-09-14 17:41:33 +0530

    admin class

---
+++ HealthcareManagementSystem/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealthcareManagementSystem.Models
+{
+    public class Admin
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+    } 
+}
