4543f1cf5dd59e455c9ca4b1ced2c8b8e087b0e4
EPPAKAYALA MOUNIKA <mounika.ep@gmail.com>
2020-09-14 17:51:28 +0530

    reception class

---
+++ HealthcareManagementSystem/Models/Reception.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthcareManagementSystem.Models
+{
+    public class Reception
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
