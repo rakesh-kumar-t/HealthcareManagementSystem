89eab677958a0a32ddbf5fa39577b469a2d7a314
Ann Liya <annliya4@gmail.com>
2020-09-14 18:15:05 +0530

    Added Context class and connection string

---
+++ HealthcareManagementSystem/Models/HealthCareContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HealthcareManagementSystem.Models
+{
+    public class HealthCareContext:DbContext
+    {
+        public HealthCareContext():base("name=Dbconfig")
+        {
+
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Reception> Receptions { get; set; }
+        public DbSet<Members> Members { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<DrugHouse> DrugHouses { get; set; }
+        public DbSet<Pharmacy> Pharmacies { get; set; }
+        public DbSet<Pharmastock> Pharmastocks { get; set; }
+    }
+}
