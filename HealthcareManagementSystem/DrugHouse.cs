fcc02f497554c8b2879d7bed772c223e1e8d54b2
EPPAKAYALA MOUNIKA <mounika.ep@gmail.com>
2020-09-14 17:04:32 +0530

    DrugHouse

---
+++ HealthcareManagementSystem/DrugHouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthcareManagementSystem
+{
+    public class DrugHouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int StockLeft { get; set; }
+        public double Price { get; set; }
+        
+    } 
+}
