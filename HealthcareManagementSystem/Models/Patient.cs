891ed40cb68845c64e73ba7961ddc252c95db943
Suchi20-git <psuchetha20@gmail.com>
2020-09-14 17:02:51 +0530

    Patientclass properties

---
+++ HealthcareManagementSystem/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthcareManagementSystem.Models
+{
+    public class Patient
+    {
+        public int id { get; set; }
+        public int opno { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string doctorname { get; set; }
+        public string disease { get; set; }
+        public float billamount { get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
