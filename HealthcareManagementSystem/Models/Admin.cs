bfab8ace4b3a9369fc9707a8f2f521de417cd923
EPPAKAYALA MOUNIKA <mounika.ep@gmail.com>
2020-09-14 17:44:07 +0530

    admin class created

--- HealthcareManagementSystem/Models/Admin.cs
+++ HealthcareManagementSystem/Models/Admin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthcareManagementSystem.Models
 {
